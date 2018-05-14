4549d3c0d9856db0f065404b2b8d8087b40bb580
Ryulth <adboy94@khu.ac.kr>
2018-05-14 16:36:55 +0900

    -ADD video frame

--- video_capture.py
+++ video_capture.py
@@ -1,11 +1,35 @@
+import sys
+import argparse
+import timeit
 import cv2
-vidcap = cv2.VideoCapture('Compton.mp4')
-success,image = vidcap.read()
-count = 0
-success = True
-while success:
-  success,image = vidcap.read()
-  cv2.imwrite("frame%d.jpg" % count, image)     # save frame as JPEG file
-  if cv2.waitKey(10) == 27:                     # exit if Escape is hit
-      break
-  count += 1
+print(cv2.__version__)
+
+def extractImages(pathIn, pathOut):
+    count = 0
+    vidcap = cv2.VideoCapture(pathIn)
+    success,image = vidcap.read()
+    success = True
+    while success:
+        vidcap.set(cv2.CAP_PROP_POS_MSEC,(count*(100/6)))    # 1000->1frame 100->10frame  100/6->60frame
+        success,image = vidcap.read()
+        #print ('Read a new frame: ', success)
+        #cv2.imwrite( pathOut + "\\frame%d.jpg" % count, image)     # save frame as JPEG file
+        count = count + 1
+    print(count)
+
+""""# if __name__=="__main__":
+    print("aba")
+    a = argparse.ArgumentParser()
+    a.add_argument("--pathIn", help="path to video")
+    a.add_argument("--pathOut", help="path to images")
+    args = a.parse_args()
+    print(args)
+    extractImages(args.pathIn, args.pathOut)
+"""
+
+pathin='./image/tempvideo1.mp4'
+pathout='./image/result'
+start = timeit.default_timer()
+extractImages(pathin,pathout)
+end = timeit.default_timer()
+print(end-start)
